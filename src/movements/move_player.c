fa57465fad651cefb060b1cffbe986c8f77d98d2
Marta Fontseré Guarro <mfontser@car10s4.42barcelona.com>
2025-03-12 01:28:48 +0100

    fix fov and movements direction

--- src/movements/move_player.c
+++ src/movements/move_player.c
@@ -6,7 +6,7 @@
 /*   By: mfontser <mfontser@student.42.barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 01:16:09 by mfontser          #+#    #+#             */
-/*   Updated: 2025/03/12 00:57:40 by mfontser         ###   ########.fr       */
+/*   Updated: 2025/03/12 01:28:33 by mfontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@ void prepare_next_position(t_game *gdata, t_vision vision, double	*move_x, doubl
 {
 	if (gdata->player.mov_up == 1) // Ir hacia adelante
 	{
-		*move_x -= MOVE_SPEED * cos(vision.vision_angle);
+		*move_x -= MOVE_SPEED * -cos(vision.vision_angle);
 		*move_y -= MOVE_SPEED * sin(vision.vision_angle);
 	}
 	if (gdata->player.mov_down == 1) // Ir hacia atrás
 	{
-		*move_x += MOVE_SPEED * cos(vision.vision_angle);
+		*move_x += MOVE_SPEED * -cos(vision.vision_angle);
 		*move_y += MOVE_SPEED * sin(vision.vision_angle); 
 	}
 	 if (gdata->player.mov_right == 1) // Moverse a la derecha
     {
         *move_x += MOVE_SPEED * cos(vision.vision_angle - M_PI_2);
-        *move_y += MOVE_SPEED * sin(vision.vision_angle - M_PI_2);
+        *move_y += MOVE_SPEED * -sin(vision.vision_angle - M_PI_2);
     }
     if (gdata->player.mov_left == 1) // Moverse a la izquierda
     {
         *move_x += MOVE_SPEED * cos(vision.vision_angle + M_PI_2);
-        *move_y += MOVE_SPEED * sin(vision.vision_angle + M_PI_2);
+        *move_y += MOVE_SPEED * -sin(vision.vision_angle + M_PI_2);
     }
 }
 
