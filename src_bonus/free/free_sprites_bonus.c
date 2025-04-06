79b69b5c8e6ecf38d65c3e4c7fa9e3edc0431d25
Marta Fontseré Guarro <mfontser@cbr7s4.42barcelona.com>
2025-04-06 19:07:24 +0200

    leaks textures

--- src_bonus/free/free_sprites_bonus.c
+++ src_bonus/free/free_sprites_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mfontser <mfontser@student.42.barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 19:25:43 by mfontser          #+#    #+#             */
-/*   Updated: 2025/04/06 13:02:36 by mfontser         ###   ########.fr       */
+/*   Updated: 2025/04/06 18:48:37 by mfontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,10 @@ void	free_dragon_sprites(t_game *gdata, t_texture *texture)
 	i = 0;
 	while (i < 4)
 	{
+		mlx_delete_xpm42(texture->dragon_img[i].xpm);
 		if (texture->dragon_img[i].data)
 		{
 			mlx_delete_image(gdata->mlx.init, texture->dragon_img[i].data);
-			mlx_delete_xpm42(texture->dragon_img[i].xpm);
 			texture->dragon_img[i].data = NULL;
 		}
 		i++;
@@ -86,10 +86,10 @@ void	free_fire_sprites(t_game *gdata, t_texture *texture)
 	i = 0;
 	while (i < 8)
 	{
+		mlx_delete_xpm42(texture->fire_img[i].xpm);
 		if (texture->fire_img[i].data)
 		{
 			mlx_delete_image(gdata->mlx.init, texture->fire_img[i].data);
-			mlx_delete_xpm42(texture->fire_img[i].xpm);
 			texture->fire_img[i].data = NULL;
 		}
 		i++;
